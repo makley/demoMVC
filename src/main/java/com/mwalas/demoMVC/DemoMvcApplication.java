d8482edd846a5b0b5073446740d0baa2073f287a
makley <makley@o2.pl>
2019-01-05 20:35:26 +0000

    Create project

---
+++ src/main/java/com/mwalas/demoMVC/DemoMvcApplication.java
@@ -0,0 +1,14 @@
+package com.mwalas.demoMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcApplication.class, args);
+	}
+
+}
+
